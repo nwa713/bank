58621b32b3097a14514d416230dc73182de6ce28
nwa713 <nwankwovictor789@gmail.com>
2025-06-17 03:33:12 -0700

    banking code

---
+++ Saving_account.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+class SavingsAccount:
+    def __init__(self):
+        self.balance = 0
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposited {amount}.New balance: {self.balance}")
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("insufficient balance")
+        else:
+            self.balance -= amount
+            print(f"Withdrawed {amount}.New balance: {self.balance}")
+
+    def check_balance(self):
+        print(f"Current balance: {self.balance}")
+
+account=SavingsAccount()
+st.title("Savings Account")
+
+st.sidebar.header("MENU")
+chioce= st.sidebar.selectbox("Choose an option",["Deposit","Withdraw","Check Balance"])
+if chioce == "Deposit":
+    amount= st.sidebar.number_input("Amount")
+    if st.button("Deposit"):
+        st.write(account.deposit(amount))
+
+elif chioce == "Withdraw":
+    amount= st.number_input("Amount")
+    if st.button("Withdraw"):
+        st.write(account.withdraw(amount))
+
+elif chioce == "Check Balance":
+    if st.button("Check Balance"):
+        st.write(account.check_balance())
